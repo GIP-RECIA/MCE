801d075cd3c9ebc71bdfc72ba090052b592d9d8b
ismail-durak <i.durak45000@gmail.com>
2026-04-13 17:16:03 +0200

    optimisation de la securité config

--- src/main/java/fr/recia/mce/api/escomceapi/configuration/SecurityConfiguration.java
+++ src/main/java/fr/recia/mce/api/escomceapi/configuration/SecurityConfiguration.java
@@ -18,10 +18,10 @@ package fr.recia.mce.api.escomceapi.configuration;
 import fr.recia.mce.api.escomceapi.configuration.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -35,18 +35,31 @@ public class SecurityConfiguration {
         this.jwtFilter = jwtFilter;
     }
 
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/health-check",
+            "/api/personne/mce/token"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**", "/health-check").permitAll()
-                .antMatchers("/api/personne/mce/token", "/api/password/**").permitAll()
-                .antMatchers("/api/personne/mce/getuser", "/api/personne/mce/**").permitAll()
-                .anyRequest().authenticated()
+
+                .antMatchers(PUBLIC_ENDPOINTS).permitAll()
+
+                // à supprimer
+                // .antMatchers("/**").permitAll()
+
+                 .anyRequest().authenticated()
+
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
