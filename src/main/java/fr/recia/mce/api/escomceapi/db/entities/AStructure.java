61b257f5089e8af6dcec8d1fb74c557c7c2c508a
ismail-durak <i.durak45000@gmail.com>
2026-05-04 16:33:26 +0200

    patch : suppression du test dans le git

--- src/main/java/fr/recia/mce/api/escomceapi/db/entities/AStructure.java
+++ src/main/java/fr/recia/mce/api/escomceapi/db/entities/AStructure.java
@@ -35,6 +35,7 @@ import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -58,10 +59,12 @@ public class AStructure implements Serializable {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contact_id")
+    @JsonIgnore
     private APersonne aPersonneByContactId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "responsable_id")
+    @JsonIgnore
     private APersonne aPersonneByResponsableId;
 
     @Temporal(TemporalType.TIMESTAMP)
@@ -105,6 +108,7 @@ public class AStructure implements Serializable {
     private Date anneeScolaire;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "aStructure")
+    @JsonIgnore
     private Set<APersonne> aPersonnes = new HashSet<>(0);
 
     // @OneToMany(fetch = FetchType.LAZY, mappedBy = "astructure")
